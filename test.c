3a0ee98ad39dfe5291793c51329b0d23cc566bbd
agalmeida <arthurgalmeida9@gmail.com>
2020-11-05 18:32:46 -0500

    Now it works

--- test.c
+++ test.c
@@ -3,10 +3,53 @@
 //Open brackets must be closed by the same type of brackets.
 //Open brackets must be closed in the correct order.
 
+#include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    printf("hello world");
-    return 0;
-    
+
+   char string[20];
+   int final=0;
+   int i;
+   printf("Enter string:");
+   scanf("%s", string);
+   printf("%s", string);
+   for (i = 0; i < 100; i++)
+   {
+      if (string[i]== '\0')
+         break;
+      else if (string[i]=='(' && string[i+1]==')'){
+         final = 1;
+         i++;
+      }
+      else if (string[i]=='[' && string[i+1]==']'){
+         final = 1;
+         i++;
+      }
+      else if (string[i]=='{' && string[i+1]=='}'){
+         final = 1;
+         i++;
+      }
+      else{
+        final = 0;
+        break;
+      }
+
+   }
+    printf("%d", final);
+   if (!final)
+      printf("false");
+   else
+      printf("true");
+
 }
+
+
+// Ex: ()({})[]
+
+//if (char == '(')
+//    stack.push(')')
+
+// Stack: ) <- Top
+
+// Queue: 
